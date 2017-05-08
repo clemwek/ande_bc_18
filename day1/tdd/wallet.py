0af9b00309087fbd1ca41e5169d4704ab5e8ce68
Clement _dhwty_ <clemwek@gmail.com>
2017-05-08 17:35:03 +0300

    Add deposit fucntion

--- day1/tdd/wallet.py
+++ day1/tdd/wallet.py
@@ -3,10 +3,19 @@
 
 class Wallet(object):
     """ Defines blue print for digital wallet """
+    def __init__(self):
+        self.balance = 0
 
-    def deposit(self):
+    def deposit(self, amount):
         """ Adds money to wallet """
-        pass
+        try:
+            self.balance += amount
+            return self.balance
+        except ValueError:
+            return 'amount must be number!'
+
+        if isinstance(amount, int):
+            return 'Numbers only please!!'
 
     def withdraw(self):
         """ Withdraws money from wallet """
