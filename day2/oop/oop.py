4d70a493863faf5addf4458ca5402b4865490ad0
Clement _dhwty_ <clemwek@gmail.com>
2017-05-09 13:42:17 +0300

    Restructured to use pep8

--- day2/oop/oop.py
+++ day2/oop/oop.py
@@ -2,63 +2,62 @@
 this ia a sample use of OOP
 """
 
-class BankAccount (object):
 
-	def __init__(self):
-		pass
+class BankAccount(object):
+    def __init__(self):
+        pass
 
-	def withdraw (self):
-		'''This is blank'''
-		pass
+    def withdraw(self):
+        """This is blank"""
+        pass
 
-	def deposit (self):
-		'''this is a blank'''
-		pass
-
-
-class SavingsAccount (BankAccount):
-
-	def __init__ (self):
-		self.balance = 500
-
-	def deposit (self, amount):
-		if amount < 0:
-			raise RuntimeError('Invalid deposit amount.')
-		else :
-			self.balance += amount
-			return balance
-
-	def withdraw (self, amount):
-		if amount < 0:
-			raise RuntimeError('Invalid deposit amount.')
-		else :
-			if amount > self.balance :
-				raise RuntimeError('Cannot withdraw beyond the current account balance.')
-			elif (self.balance-amount) < 500 :
-				raise RuntimeError('Cannot withdraw beyond the minimum account balance.')
-			else :
-				balance = self.balance - amount
-		return balance
+    def deposit(self):
+        """this is a blank"""
+        pass
 
 
-class CurrentAccount (BankAccount):
+class SavingsAccount(BankAccount):
+    def __init__(self):
+        super().__init__()
+        self.balance = 500
 
-	def __init__ (self):
-		self.balance = 0
+    def deposit(self, amount):
+        if amount < 0:
+            raise RuntimeError('Invalid deposit amount.')
+        else:
+            self.balance += amount
+            return self.balance
 
-	def deposit (self, amount):
-		if amount < 0:
-			raise RuntimeError('Invalid deposit amount.')
-		else :
-			self.balance += amount
-		return balance
+    def withdraw(self, amount):
+        if amount < 0:
+            raise RuntimeError('Invalid deposit amount.')
+        else:
+            if amount > self.balance:
+                raise RuntimeError('Cannot withdraw beyond the current account balance.')
+            elif (self.balance - amount) < 500:
+                raise RuntimeError('Cannot withdraw beyond the minimum account balance.')
+            else:
+                balance = self.balance - amount
+        return balance
 
-	def withdraw (self, amount):
-		if amount < 0:
-			raise RuntimeError('Invalid deposit amount.')
-		else :
-			if amount > self.balance :
-				raise RuntimeError('Cannot withdraw beyond the current account balance.')
-			else :
-				balance = self.balance - amount
-		return balance
+
+class CurrentAccount(BankAccount):
+    def __init__(self):
+        self.balance = 0
+
+    def deposit(self, amount):
+        if amount < 0:
+            raise RuntimeError('Invalid deposit amount.')
+        else:
+            self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount < 0:
+            raise RuntimeError('Invalid deposit amount.')
+        else:
+            if amount > self.balance:
+                raise RuntimeError('Cannot withdraw beyond the current account balance.')
+            else:
+                balance = self.balance - amount
+        return balance
