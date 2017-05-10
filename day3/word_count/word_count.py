b438353fe851a82415ce5a617c3f0f8822412c8b
Clement _dhwty_ <clemwek@gmail.com>
2017-05-10 10:23:44 +0300

    finished proj 2

---
+++ day3/word_count/word_count.py
@@ -0,0 +1,25 @@
+"""word count"""
+
+
+# recieve the text
+# split to words and save in a list
+# loop thro the list conting and saving the out
+
+def words(in_words):
+    try:
+        in_words = in_words.split()
+        dic_out = {}
+        for word in in_words:
+            if word.isdigit():
+                if int(word) in dic_out:
+                    dic_out[int(word)] = dic_out[int(word)] + 1
+                else:
+                    dic_out[int(word)] = 1
+            else:
+                if word in dic_out:
+                    dic_out[word] = dic_out[word] + 1
+                else:
+                    dic_out[word] = 1
+        return dic_out
+    except Exception as e:
+        return e
